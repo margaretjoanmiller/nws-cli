f7f6ff8f5a57dcb6c74dd5c21d69afeea9b7af36
Mae Miller <mmiller@wanderingwires.net>
2024-08-10 20:01:28 -0700

    Setting up find grid

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let echo_json: String = reqwest::Client::new()
+        .get("https://api.weather.gov/points/34.052235,-118.243683")
+        .header("User-Agent", "nws-cli-0.1")
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("{:#?}", echo_json);
+    Ok(())
 }
