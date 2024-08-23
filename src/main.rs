d25703f43632048ecef6714204a91138ca3e3747
Mae Miller <mmiller@wanderingwires.net>
2024-08-23 12:38:07 -0700

    Trying to get stuff working

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use crate::geocode;
-
 #[tokio::main]
 async fn main() {
 
     let echo_json = geocode::get_grids(34.052235, -118.243684).await;
+    match echo_json {
+    }
     println!("{:#?}", echo_json);
 
 
