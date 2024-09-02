007fcb58507e04870e4d1427678a4d98e5d0da46
Mae Miller <mmiller@wanderingwires.net>
2024-09-01 21:30:30 -0700

    Getting geocoding working

--- src/geocode.rs
+++ src/geocode.rs
@@ -2,9 +2,10 @@ pub mod geocode {
     use crate::grid_json;
 
     use reqwest::{Client,ClientBuilder,Response};
-    use serde_json::{Result, Value};
-    
-    async fn get_grids(lat: f32, long: f32) -> Result<serde_json::Value, serde_json::Error> {
+    use serde_json::{Result};
+    use crate::grid_json::grid_json::GridJson;
+
+    async fn get_grids(lat: f32, long: f32) -> anyhow::Result<GridJson> {
         let builder = reqwest::Client::builder()
             .user_agent("nws-cli-0.1")
             .build();
@@ -17,16 +18,9 @@ pub mod geocode {
         let echo_json_result = client
             .get(format!("https://api.weather.gov/points/{lat},{long}"))
             .send()
-            .await;
-
-        let json_raw = match echo_json_result {
-            Ok(val) => val,
-            Err(err) => panic!("Failed to send http, {err}")
-        };
-        
-        match json_raw.json::<serde_json::Value>().await {
-            Ok(json)
-        }
-
+            .await?
+            .json::<GridJson>()
+            .await?;
+        Ok(echo_json_result)
     }
 }
